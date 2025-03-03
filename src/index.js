bcc020fbf4be43d74f39e002d3294435abe4f951
ChakNikolay87 <ChakNikolay@Yandex.ru>
2025-03-03 09:20:14 +0300

    feat: Выполнил задание второго стринта

---
+++ src/index.js
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize.css';
+import './index.css';
+import App from './components/app/app';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/app/app';
+import { Provider } from 'react-redux';
+import { store } from './services/store';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+);
+>>>>>>> 453cb58 (feat: выполнил задание второго спринта)
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+/// <reference types="react-scripts" />
+=======
+/// <reference types="react-scripts" />
+>>>>>>> 453cb58 (feat: выполнил задание второго спринта)
