26f0495d74eb3000cd930df4f380f389401158ae
ChakNikolay87 <ChakNikolay@Yandex.ru>
2025-03-18 10:14:15 +0300

    fix: Внес изменения согласно ревью

---
+++ src/components/Ingredient-details-modal.js
@@ -0,0 +1,36 @@
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useLocation } from "react-router-dom";
+import Modal from "./modal/modal";
+import { openModal } from "../services/slices/modalSlice";
+
+const IngredientDetailsModal = ({ onClose }) => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const location = useLocation();
+
+  const { ingredients, isLoading, error } = useSelector(
+    (store) => store.ingredients
+  );
+  const ingredient = useMemo(() => {
+    if (id) {
+      return ingredients.filter((ingredient) => ingredient._id === id)[0];
+    }
+  }, [ingredients, id]);
+
+  useEffect(() => {
+    if (id && ingredient) {
+      dispatch(
+        openModal({
+          type: "INGREDIENT_DETAILS",
+          data: ingredient,
+          title: "Детали ингридиента",
+        })
+      );
+    }
+  }, [id, ingredient]);
+
+  return !isLoading && !error && <Modal onClose={onClose} />;
+};
+
+export default IngredientDetailsModal;
---
+++ src/components/modal/modal-overlay/modal-overlay.js
@@ -0,0 +1,17 @@
+import styles from "./modal-overlay.module.css";
+import PropTypes from "prop-types";
+
+const ModalOverlay = ({ onClose }) => {
+  return (
+    <div
+      className={styles.modalOverlay}
+      onClick={onClose ? onClose : null}
+    ></div>
+  );
+};
+
+ModalOverlay.propTypes = {
+  onClose: PropTypes.func,
+};
+
+export default ModalOverlay;
